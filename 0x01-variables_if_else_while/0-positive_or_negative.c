a2518b5ad23eef28825b76eff5a391eeca6931fc
omniah arafah <105656911+omniah7@users.noreply.github.com>
2022-06-03 23:54:21 +0200

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,10 +12,9 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
-		printf("is positive/n");
+		printf("%d is positive\n", n);
 	else if (n < 0)
-		printf("is negative/n");
+		printf("is negative\n", n);
 	else
-		printf("is zero/n");
-
+		printf("is zero\n", n);
 	return (0);}
