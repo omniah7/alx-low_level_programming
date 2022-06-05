5fccf94d3040f449e07ef9601c6ac7b94a7d3f2c
omniah7 <om267165@gmail.com>
2022-06-05 04:08:51 +0200

    10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - prints all possible different combinations of two digits
+ * Return: 0
+ */
+int main(void)
+{
+	int n , m;
+
+	for (n = 48 ; n <= 57 ; n++)
+	{
+		for (m = n ; m <= 57 ; m++)
+		{
+			putchar(n);
+			putchar(m);
+			if (n < 57 || m < 57)
+			{
+			putchar(',');
+			putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
