d574d7a4b0b1c2d6ff29e82737d2c38661ae7d67
omniah7 <om267165@gmail.com>
2022-06-05 05:38:52 +0200

    12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - prints all possible different combinations of two digits
+ * Return: 0
+ */
+int main(void)
+{
+	int n, m, k, l;
+
+	for (n = 0 ; n <= 9 ; n++)
+	{
+		for (m = 0 ; m <= 9 ; m++)
+		{
+			for (k = n  ; k <= 9 ; k++)
+			{
+				for(l = m + 1 ; l <= 9 ; l++)
+				{	
+					putchar(n + '0');
+					putchar(m + '0');
+					putchar(' ');
+					putchar(k + '0');
+					putchar(l + '0');
+				/* if not the last number */
+				if (n != 9 || m != 8 || k != 9 || l != 9)
+				{
+				putchar(',');
+				putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
