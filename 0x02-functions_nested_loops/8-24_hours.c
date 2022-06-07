eb576cbf12a132ab0f1716d8d6e38509e3641689
omniah arafah <105656911+omniah7@users.noreply.github.com>
2022-06-08 00:39:06 +0200

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,13 +4,13 @@
  */
 void jack_bauer(void)
 {
-	int h1, h2, m1 ,m2;
+	int h1, h2, m1, m2;
 
 	for (h1 = 0 ; h1 <= 2 ; h1++)
 	{
 		for (h2 = 0 ; h2 <= 9 ; h2++)
 		{
-			if(h1 == 2 && h2 == 4)
+			if (h1 == 2 && h2 == 4)
 				break;
 			for (m1 = 0 ; m1 < 6 ; m1++)
 			{
