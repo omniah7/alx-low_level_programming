798e630582ff299b413ff12d304233130e0c4390
omniah7 <om267165@gmail.com>
2022-06-07 17:50:45 +0200

    task2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,12 +4,17 @@
  */
 void print_alphabet_x10(void)
 {
-	int n;
+	int n, i;
 
 	n = 10;
 	while (n)
 	{
-		print_alphabet(void);
+		for (i = 97 ; i <= 122 ; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
 		n--;
 	}
+
 }
