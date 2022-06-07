254fe2e645e946492ae1ba8cb5e3c65b1b27b053
omniah7 <om267165@gmail.com>
2022-06-07 20:59:07 +0200

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,10 +6,17 @@
  */
 int print_last_digit(int n)
 {
+	int i;
+
 	if (n < 0)
 	{
-		return ((n * -1) % 10);
+		i = (n * -1) % 10;
+		_putchar(i + '0');
+		return (i);
 	}
 	else
+	{
+		_putchar(n + '0');
 		return (n % 10);
+	}
 }
