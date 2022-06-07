9e9d1151732885fd0468cb5322c518aa0b10a696
omniah7 <om267165@gmail.com>
2022-06-07 17:00:04 +0200

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - prints the alphabet, in lowercase, followed by a new line.
+ * Return: 0
+ */
+void print_alphabet(void);
+{
+	int n;
+
+	for (n = 97 ; n <= 122 ; n++) 
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
