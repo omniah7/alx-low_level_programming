574767d20cd9cdd478e0f01b252b3fc091b37213
omniah7 <om267165@gmail.com>
2022-06-08 00:37:14 +0200

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,12 +10,12 @@ void jack_bauer(void)
 	{
 		for (h2 = 0 ; h2 <= 9 ; h2++)
 		{
+			if(h1 == 2 && h2 == 4)
+				break;
 			for (m1 = 0 ; m1 < 6 ; m1++)
 			{
 				for (m2 = 0 ; m2 <= 9 ; m2++)
 				{
-					if(h1 == 2 && h2 == 4)
-						break;
 					_putchar(h1 + '0');
 					_putchar(h2 + '0');
 					_putchar(':');
