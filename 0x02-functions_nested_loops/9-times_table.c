545e68855e1648b9a553357218d0f87810ac296f
omniah arafah <105656911+omniah7@users.noreply.github.com>
2022-06-09 00:54:07 +0200

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,15 +12,18 @@ void times_table(void)
 		{
 			d1 = (i * n) / 10;
 			d2 = (i * n) % 10;
+			/* if it's not one digit and not the first product */
 			if (d1 != 0)
 				_putchar(d1 + '0');
+			else if (i == 0 )
+				continue;
+			else
+				_putchar(' ');
 			_putchar(d2 + '0');
 			if (i != 9)
 			{
 				_putchar(',');
 				_putchar(' ');
-				if (d1 == 0)
-					_putchar(' ');
 			}
 		}
 		_putchar('\n');
