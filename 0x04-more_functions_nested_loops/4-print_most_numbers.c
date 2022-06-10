8868eef5f63a3679d39a0e7833169e5fcb0304e8
omniah7 <om267165@gmail.com>
2022-06-11 01:25:23 +0200

    files 4-7

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9,except 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 50; i != 52)
+			_putchar(i + '0');
+	}
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_diagonal - draws a diagonal line on the terminal
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 1; i <= n; i++)
+	{
+		_putchar('\');
+		_putchar('\n');
+	}
+}
