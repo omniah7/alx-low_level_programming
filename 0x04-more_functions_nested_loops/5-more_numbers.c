8815f6e93cbf501966c288e5dac86abc5f118e1c
omniah arafah <105656911+omniah7@users.noreply.github.com>
2022-06-11 01:54:11 +0200

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,11 +6,11 @@ void more_numbers(void)
 {
 	int i, n;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (n = 0; n <= 14; n++)
 		{
-			if(n >= 10)
+			if (n >= 10)
 				_putchar(n / 10 + '0');
 			_putchar(n % 10 + '0');
 		}
