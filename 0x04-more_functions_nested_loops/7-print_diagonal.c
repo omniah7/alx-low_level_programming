195bdfcf44e1ac2d566eda30395846f5463289d0
omniah arafah <105656911+omniah7@users.noreply.github.com>
2022-06-11 02:01:24 +0200

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
  * print_diagonal - draws a diagonal line on the terminal
+ * @n: number of \ printed
  */
 void print_diagonal(int n)
 {
 	int i;
 
-	for (i = 1; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
 		_putchar('\');
 		_putchar('\n');
