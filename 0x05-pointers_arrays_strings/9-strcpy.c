34bcd4a400b6908183792c828e7a9a2f3b55599c
omniah arafah <105656911+omniah7@users.noreply.github.com>
2022-06-14 19:46:30 +0200

    Create 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * *_strcpy - copies a string into another
+ * @src: the source string pointer
+ * @dest: the target string pointer
+ *
+ * Description: a function that copies the string pointed to by src,
+ * including the terminating null byte (\0), to the buffer pointed to by dest
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	while (0)
+	{
+		*dest = *str;
+		if (str == '\0')
+			break;
+		dest++;
+		str++;
+	}
+}
