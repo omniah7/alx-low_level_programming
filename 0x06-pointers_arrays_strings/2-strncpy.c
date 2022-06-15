7cfed38cfb3351a31275fb5cdd926ed89549e97e
omniah arafah <105656911+omniah7@users.noreply.github.com>
2022-06-16 01:02:52 +0200

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,7 +10,13 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; (i < n) && (src[i] != '\0'); i++)
 		dest[i] = src[i];
+	do {
+		i++;
+		dest[i] = '\0';
+	} while (i < n)
+		
+	
 	return (dest);
 }
