3b7a3e0684fdbc0d33c34e6580864822631d2973
omniah arafah <105656911+omniah7@users.noreply.github.com>
2022-06-16 01:58:13 +0200

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,11 +7,13 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i, sum;
-	
-	sum = 0;
+
+	dif = 0;
 	for (i = 0; (s1[i] != '\0') || (s2[i] != '\0'); i++)
 	{
-		sum = s1[i] - s2[i];
+		dif = s1[i] - s2[i];
+		if (s1[i] != s2[i])
+			return (dif);
 	}
-	return (sum);
+	return (dif);
 }
