5b864a3a68d446276a5cda0a8931079c40211ad4
omniah arafah <105656911+omniah7@users.noreply.github.com>
2022-06-16 21:46:32 +0200

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,25 +7,25 @@
 char *cap_string(char *p)
 {
 	int i, j;
-	char sep[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '(', ')', '{', '}'}
+	char sep[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '(', ')', '{', '}'};
 
 	for (i = 0; p[i] != '\0'; i++)
 	{
 		/* for the first character */
 		if (i == 0 && p[i] >= 'a' && p[i] <= 'z')
 			p[i] -= 32;
-			/* comparing the string with the separators */
-			for (j = 0; sep[j] != '\0'; j++)
+		/* comparing the string with the separators */
+		for (j = 0; sep[j] != '\0'; j++)
+		{
+			if (p[i] == sep[j])
 			{
-				if (p[i] == sep[j])
-				{
-					if (p[i + 1] >= 'a' && p[i + 1] <= 'z')
-					{
-						p[i++ + 1] -= 32;
-						break;
-					}
+				if (p[i + 1] >= 'a' && p[i + 1] <= 'z')
+				{	
+					p[i++ + 1] -= 32;
+					break;
 				}
 			}
+		}
 	}
 	return (p);
 }
