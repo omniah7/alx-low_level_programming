5866a512c7ac58b8244ae59890ea0e9ece81215b
omniah arafah <105656911+omniah7@users.noreply.github.com>
2022-06-21 00:25:49 +0200

    Create 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,24 @@
+/**
+ * int _strspn - gets the length of a prefix substring
+ * @s: pointer to the string
+ * @accept: pointer to characters to be matched
+ * Return: number of bytes in the initial segment of s
+ * which consist only of bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j, b;
+
+	b = 0;
+	for (i = 0, s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				break;
+		}
+		if (accept[j] == '\0')
+			return (b);
+		b++;
+	}
+}
