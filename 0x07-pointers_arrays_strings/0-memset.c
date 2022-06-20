55c4b12c69ba14a6f5d220d8dd2556124fe7bdaa
omniah arafah <105656911+omniah7@users.noreply.github.com>
2022-06-20 22:30:58 +0200

    Create 0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+/**
+ * *_memset - fills memory with a constant byte
+ * @s: char pointer
+ * @b: the constant byte
+ * @n: number of first bytes of the memory area pointed to by s
+ * Description: The _memset() function fills the first n bytes of the memory
+ * area pointed to by s with the constant byte b
+ * Return: a pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+}
