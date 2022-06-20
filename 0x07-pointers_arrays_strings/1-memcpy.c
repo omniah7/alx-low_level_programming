edf96c794605113a942b1334c24022ec90790f02
omniah arafah <105656911+omniah7@users.noreply.github.com>
2022-06-20 22:58:21 +0200

    Create 1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+/**
+ * *_memset - fills memory with a constant byte
+ * @dest: the copied-to pointer
+ * @src: the copied-from pointer
+ * @n: number of first bytes of the memory area pointed to by src
+ * Description: The _memcpy() function copies n bytes from memory area src to memory area dest
+ * Return: a pointer to dest
+ */
+*_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
