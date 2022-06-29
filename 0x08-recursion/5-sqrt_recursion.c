2c32a7f9a1dcde0990d4894c85ee36580b04af04
omniah arafah <105656911+omniah7@users.noreply.github.com>
2022-06-30 00:03:43 +0200

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,14 +2,14 @@
 /**
  * _sqrt_recursion - returns the natural square root of a number
  * @n: the number
- * Return: the natural square root if exists. -1 if not 
+ * Return: the natural square root if exists. -1 if not
  */
 int _sqrt_recursion(int n)
 {
 	return (square(n, 1));
 }
 /**
- * sqrt - returns the natural square root of a number
+ * square - returns the natural square root of a number
  * @n: the number
  * @g: a guess number
  * Return: the natural square root if exists. -1 if not
