1f2bd91bcfc44ce9ca78080f9f8b138038ca0f08
omniah7 <om267165@gmail.com>
2022-06-30 22:06:53 +0200

    creating the dir

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * create_array - creates and initializes an array
+ * @size:the size of the array
+ * @c: a character
+ * Description: this function creates an array of chars, and initializes it
+ * 		with a specific char
+ * Return: a pointer to the array, or NULL if it fails
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+	int i;
+
+	p = malloc(size * sizeof(char));
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			p[i] = c;
+		}
+		return (p);
+	}
+	else
+		return (NULL);
+}
