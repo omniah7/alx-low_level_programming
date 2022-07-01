f4be3ffcbc8bbca726e5712088fb562c4b3e7955
omniah arafah <105656911+omniah7@users.noreply.github.com>
2022-07-02 01:15:00 +0200

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,16 +3,19 @@
 /**
  * malloc_checked - allocates memory using malloc
  * @b: an unsigned int
+ * Description: if malloc fails, the malloc_checked function should
+ * cause normal process termination with a status value of 98
  * Return: a pointer to the allocated memory
  */
  void *malloc_checked(unsigned int b)
- {
- 	void *p;
- 
- 	if (b > 0)
- 	{
- 		p = malloc(b);
- 		return (b);
- 	}
- 	_exit(98);
- }
+{
+	void *p;
+
+	p = malloc(b);
+
+	if (p != NULL)
+		return (b);
+
+	exit(98);
+}
+
