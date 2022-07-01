8a6a451be68f8f9bac9c3727c4ad9c4e87471392
omniah arafah <105656911+omniah7@users.noreply.github.com>
2022-07-01 02:32:33 +0200

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -6,23 +6,23 @@
  * memory, which contains a copy of the string given as a parameter
  * Return: a pointer to the duplicated string NULL otherwise
  */ 
- char *_strdup(char *str)
- {
- 	char *p;
- 	int i;
- 
- 	for (i = 1; *str != '\0'; i++)
- 		;
- 	p = malloc(i * sizeof(char));
- 	if (p != NULL)
- 	{
- 		while (i >= 0)
- 		{
- 			p[i] = str[i];
- 			i--;
- 		}
- 		return (p);
- 	}
- 	else
+char *_strdup(char *str)
+{
+	char *p;
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	p = malloc(i * sizeof(char));
+	if (p != NULL)
+	{
+		while (i >= 0)
+		{
+			p[i] = str[i];
+			i--;
+		}
+		return (p);
+	}
+	else
 		return (NULL);
- }
+}
