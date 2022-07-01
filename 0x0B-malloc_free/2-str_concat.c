5ab198188e5a2c2cae60b8b3d1190a89b5c3c901
omniah arafah <105656911+omniah7@users.noreply.github.com>
2022-07-01 11:36:29 +0200

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,15 +14,17 @@ char *str_concat(char *s1, char *s2)
 	char *p;
 	int i, j;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	for (i = 0; s1[i] != '\0'; i++)
 		;
 	for (j = 0; s2[j] != '\0'; j++)
 		;
-	if (s1 != NULL || s2 != NULL)
+	p = malloc((i + j + 1) * sizeof(char));
+	if (p != NULL)
 	{
-		p = malloc((i + j + 1) * sizeof(char));
-		if (p == NULL)
-			return (NULL);
 		j += i;
 		for (--i; j >= 0; j--)
 		{
