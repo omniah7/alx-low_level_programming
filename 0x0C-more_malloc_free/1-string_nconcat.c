b7452edcd3ddd7f11c7c970038a1feb9b39a500b
omniah arafah <105656911+omniah7@users.noreply.github.com>
2022-07-02 16:58:50 +0200

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,9 +17,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i,j;
 	char *s;
 
-	if (s2 == NULL)
-		s1 = "";
 	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
 		s2 = "";
 	s = malloc((length(s1) + length(s2) + 1) * sizeof(char));
 	if (s != NULL)
