ee2fb096d07d089845838d4870aa05480c62795a
omniah arafah <105656911+omniah7@users.noreply.github.com>
2022-07-02 02:35:59 +0200

    Create 1-string_nconcat.c

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: number of the first bytes of s2
+ * Description: returns a pointer to a newly allocated space in
+ * memory, which contains s1, followed by the first n bytes of s2,
+ * and null terminated
+ * If n is greater or equal to the length of s2 then use the entire string s2
+ * if NULL is passed, treat it as an empty string
+ * Return: a pointer to the concatenated string.  Null if fails
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+{
+	unsigned int i,j;
+	char *s;
+
+	s = malloc((length(s1) + length(s2) + 1) * sizeof(char));
+	if (s != NULL)
+	{
+		for (i = 0; s1[i] != '\0'; i++)
+			s[i] = s1[i];
+		if (n >= length(s2))
+		{
+			for (j = 0; s2[j] != '\0'; j++)
+				s[j + i] = s2[j];
+			s[j + i] = '\0';
+			return (s);
+		}
+		else
+		{
+			for (j = 0; j < n; j++)
+				s[j + i] = s2[j];
+			return (s);
+		}
+	}
+	return (NULL);
+}
+/**
+ * length - finds the length of a string
+ * @str: a pointer to the string
+ * Return; the length of str
+ */
+int length(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	;
+	return (i);
+}
