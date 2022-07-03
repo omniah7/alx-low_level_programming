d516f4de78491eaf7bb63a06e6bedd5c2bcacc06
omniah arafah <105656911+omniah7@users.noreply.github.com>
2022-07-03 19:14:56 +0200

    Create 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - multiplies two numbers
+ * @argc: arguments count
+ * @argv: arguments vector
+ * Return: 0 on success. 1 if fails
+ */
+int main(int argc, char **argv)
+{
+	if ((argc - 1) == 2)
+	{
+		printf("%i\n",(int)argv[1] * (int)argv[2]);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
