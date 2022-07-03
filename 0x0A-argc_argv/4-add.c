9ebc64fc74ce10a62742500fe8a1b1b8670b526d
omniah arafah <105656911+omniah7@users.noreply.github.com>
2022-07-03 22:29:08 +0200

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -27,15 +27,15 @@ int main(int argc, char **argv)
 	int sum, i;
 
 	for (sum = 0, i = 1; i < argc; i++)
+	{
+		if (check(argv[i]))
+			sum += atoi(argv[i]);
+		else
 		{
-			if (check(argv[i]))
-				sum += atoi(argv[i]);
-			else
-			{
-				printf("Error\n");
-				return (1);
-			}
+			printf("Error\n");
+			return (1);
 		}
+	}
 	printf("%i\n", sum);
 	return (0);
 }
