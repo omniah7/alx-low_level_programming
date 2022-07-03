a346d6b97e75cd95cbff0632b99bac79ce05dac5
omniah arafah <105656911+omniah7@users.noreply.github.com>
2022-07-03 19:03:46 +0200

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,14 +2,14 @@
 /**
  * main - prints all arguments it receives
  * @argc: arguments count
- * @argc: arguments vector
+ * @argv: arguments vector
  * Return: 0 on success
  */
 int main(int argc, char **argv)
 {
 	int i;
 
-	for (i = 1; i < argc; i++)
+	for (i = 0; i < argc; i++)
 		printf("%s\n", argv[i]);
 	return (0);
 }
