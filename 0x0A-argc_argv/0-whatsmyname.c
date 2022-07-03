92d56cca838f4c31aafc68d6887692f1ae9ba826
omniah arafah <105656911+omniah7@users.noreply.github.com>
2022-07-03 18:44:18 +0200

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,11 +2,11 @@
 /**
  * main - prints the program's name, followed by a new line
  * @argc: arguments count
- * @argc: arguments vector
+ * @argv: arguments vector
  * Return: 0 on success
-*/
+ */
 int main(int argc, char **argv)
 {
-	printf("%s\n", argv[argc -1]);
+	printf("%s\n", argv[argc - 1]);
 	return (0);
 }
