20b326a22d6e0f8e7a9e82ab65a2e19d279ddecb
omniah arafah <105656911+omniah7@users.noreply.github.com>
2022-07-03 20:20:23 +0200

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,7 +10,7 @@ int main(int argc, char **argv)
 {
 	if ((argc - 1) == 2)
 	{
-		printf("%i\n",(atoi(argv[1]) * atoi(argv[2]));
+		printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
 		return (0);
 	}
 	else
