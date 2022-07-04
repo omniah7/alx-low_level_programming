4e69737cbf5db3c11a7c76a3736baa43e868b00a
omniah arafah <105656911+omniah7@users.noreply.github.com>
2022-07-05 01:56:02 +0200

    Create dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - defines a struct dog
+ * @name: a char pointer to the name of dog
+ * @age: a float number of the age of dog
+ * @owner: a char pointer to the owner of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
