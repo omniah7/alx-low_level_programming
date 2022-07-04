ad24760294dd3a9691556c9d2246379f354bf708
omniah arafah <105656911+omniah7@users.noreply.github.com>
2022-07-05 01:14:15 +0200

    Create dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,12 @@
+/**
+ * struct dog - defines a dog struct
+ * @name: the name of dog
+ * @age: the age of dog
+ * @owner: the owner of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
