4623dad14475cf45f088daa7d8bf416c0b07149a
omniah arafah <105656911+omniah7@users.noreply.github.com>
2022-07-05 05:40:14 +0200

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -5,7 +5,7 @@
 * @name: a pointer to the name of dog
 * @age: the float number of age
 * @owner: a pointer to the owner  name
-*Return: a pointer to the new dog
+* Return: a pointer to the new dog
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
@@ -15,7 +15,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (name == NULL || age < 0 || owner == NULL || dog == NULL)
 	{
-		free (dog);
+		free(dog);
 		return (NULL);
 	}
 
