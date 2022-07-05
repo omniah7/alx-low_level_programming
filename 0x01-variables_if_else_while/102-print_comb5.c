f3baa2bc4585f2fefee970b8596e5903f59fdc51
omniah arafah <105656911+omniah7@users.noreply.github.com>
2022-07-05 21:45:50 +0200

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,8 +6,9 @@
  * All numbers should be printed with two digits. 1 should be printed as 01
  * The combination of numbers must be separated by comma, followed by a space
  * The combinations of numbers should be printed in ascending order
- * 00 01 and 01 00 are considered as the same combination of the numbers 0 and 1
- * You can only use the putchar function (every other function (printf, puts, etc…) is forbidden)
+ * (00 01) and (01 00) are considered as
+ * the same combination of the numbers 0 and 1
+ * You can only use the putchar function
  * You can only use putchar eight times maximum in your code
  * You are not allowed to use any variable of type char
  * Return: 0
