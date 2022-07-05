e2394072e03bbe2870f3ffac07f49d45e2556134
omniah arafah <105656911+omniah7@users.noreply.github.com>
2022-07-05 19:29:15 +0200

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 /**
- * main - prints all possible different combinations of two digits
+ * main - prints all possible different combinations of two two-digits
+ * Description: The numbers should range from 0 to 99
+ * The two numbers should be separated by a space
+ * All numbers should be printed with two digits. 1 should be printed as 01
+ * The combination of numbers must be separated by comma, followed by a space
+ * The combinations of numbers should be printed in ascending order
+ * 00 01 and 01 00 are considered as the same combination of the numbers 0 and 1
+ * You can only use the putchar function (every other function (printf, puts, etc…) is forbidden)
+ * You can only use putchar eight times maximum in your code
+ * You are not allowed to use any variable of type char
  * Return: 0
  */
 int main(void)
@@ -12,25 +21,30 @@ int main(void)
 		for (m = 0 ; m <= 9 ; m++)
 		{
 			for (k = 0  ; k <= 9 ; k++)
-			{if ( n <= k && m <= l){
-				for (l = 0 ; l <= 9 ; l++)
+			{
+				if ( n <= k && m <= l)
 				{
-					if (!(n == k && m == l)){
-                                        if ( n <= k && m <= l)
+					for (l = 0 ; l <= 9 ; l++)
 					{
-						putchar(n + '0');
-						putchar(m + '0');
-						putchar(' ');
-						putchar(k + '0');
-						putchar(l + '0');
-						/* if not the last number */
-						if (n != 9 || m != 8 || k != 9 || l != 9)
+						if (!(n == k && m == l))
 						{
-						putchar(',');
-						putchar(' ');
+                                   			if ( n <= k && m <= l)
+							{
+								putchar(n + '0');
+								putchar(m + '0');
+								putchar(' ');
+								putchar(k + '0');
+								putchar(l + '0');
+								/* if not the last number */
+								if (!(n == 9 && m == 8 && k == 9 && l == 9))
+								{
+									putchar(',');
+									putchar(' ');
+								}
+							}
 						}
-					}}
-				}}
+					}
+				}
 			}
 		}
 	}
