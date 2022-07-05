4c39995d1d068891c36e2ed5f1da162e022b74be
omniah arafah <105656911+omniah7@users.noreply.github.com>
2022-07-06 01:38:05 +0200

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	unsigned long int j, k, sum;
+	unsigned long long int j, k, sum;
 	int i;
 
 	j = 1;
@@ -21,7 +21,7 @@ int main(void)
 		sum = j + k;
 		j = k;
 		k = sum;
-		printf("%lu", sum);
+		printf("%llu", sum);
 		if (i != 95)
 			printf(", ");
 		i++;
