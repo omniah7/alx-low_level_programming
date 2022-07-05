bf8cd1fe89c278d60ca33034962b7155b5ea5886
omniah arafah <105656911+omniah7@users.noreply.github.com>
2022-07-05 18:49:23 +0200

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,10 +7,12 @@ int main(void)
 {
 	int n;
 
-	for (n = 48 ; n <= 57 ; n++)
+	for (n = '0'; n <= '9'; n++)
 		putchar(n);
-	for (n = 97 ; n <= 102 ; n++)
+
+	for (n = 'a'; n <= 'f'; n++)
 		putchar(n);
+
 	putchar('\n');
 	return (0);
 }
