40ce3d425161e378f92f094f05b495349960df5c
omniah arafah <105656911+omniah7@users.noreply.github.com>
2022-07-05 18:40:10 +0200

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,8 +10,8 @@ int main(void)
 	for (i = 'a'; i <= 'z'; i++)
 		putchar(i);
 
-	for (j = 'A' ; j <= 'Z' ; j++)
-		putchar(j);
+	for (i = 'A'; i <= 'Z'; i++)
+		putchar(i);
 
 	putchar('\n');
 	return (0);
