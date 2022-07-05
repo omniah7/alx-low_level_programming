16098611c28ab93c4029f3a87f5fd24adb2549bb
omniah arafah <105656911+omniah7@users.noreply.github.com>
2022-07-05 05:36:49 +0200

    Create 5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+* free_dog - frees dogs
+* @d: a pointer to a dog struct
+*/
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
