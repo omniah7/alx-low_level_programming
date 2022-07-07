447b663e6c03384382ff62dffe31c4241278c153
omniah arafah <105656911+omniah7@users.noreply.github.com>
2022-07-07 22:35:37 +0200

    Create 3-main.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,45 @@
+#include "3-get_op_func.c"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - prints the result of the operation, followed by a new line
+* @argc: arguments count
+* @argv: argument vector
+*
+* Description: if the number of arguments is wrong, print Error
+* and exit with the status 98
+* Otherwise print Error, and exit with the status 99
+* if the user tries to divide (/ or %) by 0,
+* print  * ** Error, and exit with the status 100
+* Return: 0
+*/
+int main(int argc, char **argv)
+{
+	int a, b;
+	char *o;
+	
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	o = argv[2];
+
+	if (b == 0 && (*o == '/' || *o == '%'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	if (get_op_func(o) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	else
+		printf("%i\n", get_op_func(o)(a, b));
+	return (0);
+}
