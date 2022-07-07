2c979d05ba8bd18d5a99102d0864121b41c0d4da
omniah arafah <105656911+omniah7@users.noreply.github.com>
2022-07-07 16:22:32 +0200

    Update 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -4,15 +4,14 @@
 * @array: an int array pointer
 * @size: the size of the array
 * @cmp:  a pointer to the function to be used to compare values
-*
-* Description: returns the index of the first element for which
+* Return: the index of the first element for which
 * the cmp function does not return 0
-* -1 if no element matches or if size <= 0
-* Return: the index of number that matched. -1 otherwise
+* -1 if no element matches or if size <= 0 otherwise
 */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
+
 	if (!(array == NULL || cmp == NULL))
 		for (i = 0; i < size; i++)
 		{
