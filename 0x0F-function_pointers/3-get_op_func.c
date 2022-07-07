c2bb7e607278012a42538ff479833b9d8d826605
omniah arafah <105656911+omniah7@users.noreply.github.com>
2022-07-07 22:38:20 +0200

    Create 3-get_op_func.c

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,25 @@
+#include "3-op_functions.c"
+/**
+* (*get_op_func) - selects the correct function to perform
+* @s: a pointer to the operator
+* Return: a pointer to the function required
+*/
+int (*get_op_func(char *s))(int, int)
+{
+	int i;
+	op_t ops[] = {
+					{"+", op_add},
+					{"-", op_sub},
+					{"*", op_mul},
+					{"/", op_div},
+					{"%", op_mod},
+					{NULL, NULL}
+					};
+
+	for (i = 0; i < 6; i++)
+	{
+		if (*s == *(ops[i].op))
+			return (ops[i].f);
+	}
+	return (NULL);
+}
