763ecc3ece2aab5b74756a1fa31678f7c809fb02
omniah arafah <105656911+omniah7@users.noreply.github.com>
2022-07-08 14:47:46 +0200

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,6 +4,15 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+void strFunc(va_list list);
+void charFunc(va_list list);
+void floatFunc(va_list list);
+void intFunc(va_list list);
+typedef struct type{
+			char*;
+			void (*f)(va_list);
+	}FunType;
 
 #endif
 
