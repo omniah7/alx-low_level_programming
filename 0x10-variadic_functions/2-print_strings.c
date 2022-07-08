d9f5b0f9c433633d3068db5f8cc2b5a62becb535
omniah arafah <105656911+omniah7@users.noreply.github.com>
2022-07-08 05:19:24 +0200

    Update 2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -14,7 +14,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(list, n);
 	for (i = 0; i < n; i++)
 	{
-		s = va_arg(list, char*)
+		s = va_arg(list, char*);
 		if (s == NULL)
 			printf("(nil)");
 		else
