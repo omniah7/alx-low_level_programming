8deb076d3bd00e220902bf96a91709d52bce7743
omniah7 <om267165@gmail.com>
2022-07-15 08:44:18 -0700

    creating file 2 and editiny betty style for file 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,18 +1,14 @@
 #include "lists.h"
 
 /**
- *
- * * print_list - returns number of elements of a list_t list
- *
- * * @h: a pointer to a list
- *
- * * Return: number of nodes
- *
- * */
-
+ * list_len - returns number of elements of a list_t list
+ * @h: a pointer to a list
+ * Return: number of nodes
+ */
 size_t list_len(const list_t *h)
 {
 	size_t elements = 0;
+
 	while (h != NULL)
 	{
 		h = h->next;
---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+ * add_node - adds a new node at the beginning of a list_t list
+ * @h: array of list_t pointers
+ * @str: a string
+ * Return: the address of the new element, or NULL if it failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	unsigned int l;
+	char *dup = strdup(str);
+
+	if (dup == NULL || head[0] == NULL)
+	{
+		return (NULL);
+	}
+	for (l = 0; dup[l] != '\0'; l++)
+		;
+	new = malloc(sizeof(list_t));
+	new->str = dup;
+	new->len = l;
+	new->next = head[0];
+	return (new);
+	
+}
+
