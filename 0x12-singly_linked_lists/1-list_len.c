b12b9b5385789f1124d333bbd108f03719fb3a9e
omniah7 <om267165@gmail.com>
2022-07-15 07:39:22 -0700

    creating file 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ *
+ * * print_list - returns number of elements of a list_t list
+ *
+ * * @h: a pointer to a list
+ *
+ * * Return: number of nodes
+ *
+ * */
+
+size_t list_len(const list_t *h)
+{
+	size_t elements = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		elements++;
+	}
+		return (elements);
+
+}
+
