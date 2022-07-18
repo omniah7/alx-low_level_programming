05ea08b98127db858f820e202c42878fdd94b34b
omniah arafah <105656911+omniah7@users.noreply.github.com>
2022-07-18 05:02:38 +0200

    Create 4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+* free_list - rees a list_t list
+* @head: the first element of the list
+*/
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = tmp->next;
+		free(tmp);
+	}
+}
+
