d0964a5e79e7f55d0460a4fa6bad7d2a3be7cc41
omniah7 <om267165@gmail.com>
2022-07-19 21:53:50 +0200

    assigning head with the new element

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -10,12 +10,13 @@ listint_t* add_nodeint(listint_t** head, const int n)
 {
 	listint_t *new = malloc(sizeof(listint_t));
 
-	if (new == NULL || head == NULL)
+	if (new == NULL)
 	{ 
 		free(new);
 		return (NULL);
 	}
 	new->next = *head;
 	new->n = n;
+	*head = new;
 	return(new);
 }
