f8d00e75eb5dfb639c803bf279aa7c50bb4d4baf
omniah7 <om267165@gmail.com>
2022-07-20 01:55:36 +0200

    adding a special case

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,10 +10,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	listint_t *tmp = head;
 
-	if (head == NULL)
-		return (NULL);
-
 	while (index--)
+	{
+		if (tmp == NULL)
+			return (NULL);
 		tmp = tmp->next;
+	}
 	return (tmp);
 }
