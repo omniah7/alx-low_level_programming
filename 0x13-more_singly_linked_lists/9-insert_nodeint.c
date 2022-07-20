bc978a9957762243824a3e1880144e78dd6c0911
omniah7 <om267165@gmail.com>
2022-07-20 03:44:46 +0200

    betty task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,8 +30,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			if (node == NULL)
 			{
-					free(new);
-					return (NULL);
+				free(new);
+				return (NULL);
 			}
 			node = node->next;
 		}
