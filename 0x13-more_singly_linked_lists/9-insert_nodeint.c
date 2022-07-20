e33200d0c17ea7eed2977efd1049d85b5fac9792
omniah7 <om267165@gmail.com>
2022-07-20 18:26:54 +0200

    adding a case for null head

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 #include <stdlib.h>
-#/**
+/**
 * insert_nodeint_at_index - inserts a new node at a given position
 * @head: the head of the list
 * @idx: the index of the list where the new node should be added
@@ -9,11 +9,11 @@
 */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *node = *head, *holder;
+	listint_t *node = *head;
 	listint_t *new = malloc(sizeof(listint_t));
 	unsigned int i;
 
-	if (new == NULL || *head == NULL || head == NULL)
+	if (new == NULL)
 	{
 		free(new);
 		return (NULL);
@@ -35,9 +35,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			}
 			node = node->next;
 		}
-		holder = node->next;
+		new->next = node->next;
 		node->next = new;
-		new->next = holder;
 	}
 	return (new);
 }
