0aa18775534a83df5e388a97e9e50d0deff090b6
omniah7 <om267165@gmail.com>
2022-07-22 03:43:04 +0200

    for task 3

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,6 +6,6 @@ void print_binary(unsigned long int n);
 unsigned int expo_to_bin(unsigned int n, unsigned int base, unsigned int index);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
-
+int set_bit(unsigned long int *n, unsigned int index);
 
 #endif
