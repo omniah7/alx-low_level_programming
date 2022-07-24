1557f10b0e214f1ee99be11bab5499f08497c1ec
omniah7 <om267165@gmail.com>
2022-07-25 01:09:37 +0200

    betty task 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -3,7 +3,7 @@
 * flip_bits - returns the number of bits to flip to get from number to another
 * @n: first number
 * @m: second number
-* Return: returns the number of bits you would need 
+* Return: returns the number of bits you would need
 * to flip to get from one number to another
 */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
