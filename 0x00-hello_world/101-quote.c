0c59e7833acd187b51f79822ef55ef6d0a45a77e
omniah arafah <105656911+omniah7@users.noreply.github.com>
2022-07-25 05:18:46 +0200

    Betty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,10 +4,13 @@
 * main - prints to standard error
 * Return: 1
 */
-int main (void)
+int main(void)
 {
-	int length = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", length);
+	int length = strlen("and that piece of art is useful
+\" - Dora Korpar, 2015-10-19\n");
+
+	write(2, "and that piece of art is useful
+\" - Dora Korpar, 2015-10-19\n", length);
 	return (1);
 }
 
