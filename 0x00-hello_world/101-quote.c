6b08e1503acedcde00819041ca07a79f695580d9
omniah arafah <105656911+omniah7@users.noreply.github.com>
2022-07-25 05:14:02 +0200

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,14 @@
-#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+/**
+* main - prints to standard error
+* Return: 1
+*/
+int main (void)
+{
+	int length = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", length);
+	return (1);
+}
+
 
