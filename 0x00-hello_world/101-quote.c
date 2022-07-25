e10aee8b0fe254f3a2052f8f64f26220f6c198ea
omniah arafah <105656911+omniah7@users.noreply.github.com>
2022-07-25 05:25:37 +0200

    Edit the over-80-lines

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,11 +6,13 @@
 */
 int main(void)
 {
-	int length = strlen("and that piece of art is useful
-\" - Dora Korpar, 2015-10-19\n");
+	int length = strlen(
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
-	write(2, "and that piece of art is useful
-\" - Dora Korpar, 2015-10-19\n", length);
+	write(2,
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+	, length);
+
 	return (1);
 }
 
