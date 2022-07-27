9a3c362bde2ea92f55c37c4a93e03b35f9ce2e38
omniah7 <om267165@gmail.com>
2022-07-27 16:40:41 +0200

    for task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+* read_textfile - reads a text file and prints it to the POSIX standard output
+* @filename: a text-file name
+* @letters: the number of letters it should read and print
+* Return: the actual number of letters it could read and print
+*		  0 if file can not be opened or read,or filename is NULL
+*		  0 if write fails or does not write the expected amount of bytes
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, _read, _write;
+	char *buf;
+	fd = open(filename, O_CREAT | O_RDONLY);
+	if (fd == -1)
+	{
+		close(fd);
+		return (0);
+	}
+	buf = malloc(sizeof(size_t) * letters);
+	_read = read (fd, buf, letters);
+	if (_read == -1)
+	{
+		free(buf);
+		close(fd);
+		return(0);
+	}
+	_write = write(STDOUT_FILENO, buf, _read);
+	if (_write == -1)
+	{
+		free(buf);
+		close(fd);
+		return(0);
+	}
+	return (_write);
+}
+
+
