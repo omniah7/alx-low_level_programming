76cf865854f39d2a695ca19b01b8a4ddd9665b45
omniah7 <om267165@gmail.com>
2022-07-29 21:58:49 +0200

    for task 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - copies the content of a file to another file
+* @ac: argument count
+* @av: argument vector
+* Return: 0
+*/
+int main(int ac, char **av)
+{
+	int  _write, i;
+	int fd[2], _close[2];
+	char buf[1024];
+	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	
+	if (ac != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	fd[0] = open(av[1], O_RDONLY);
+	if (fd[0] == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+
+	fd[1] = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, mode);
+	read(fd[0], buf, 1024);
+	_write = write(fd[1], buf, 1024);
+	if (_write == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+
+	_close[0] = close(fd[0]);
+	_close[1] = close(fd[1]);
+	for (i = 0; i < 2; i++)
+	{
+		if (_close[i] == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd[i]);
+			exit(100);
+		}
+	}
+	return (0);
+}
