6f9663a823eba6f52539b13743c30cdcf51832d1
omniah7 <om267165@gmail.com>
2022-07-29 15:18:06 +0200

    added a case for null text-content

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,14 +10,17 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, _write;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_APPEND | O_WRONLY);
 	if (fd == -1)
 		return (-1);
-	_write = write(fd, text_content, strlen(text_content));
-	if (_write == -1)
-		return (-1);
+	if (text_content != NULL)
+	{
+		_write = write(fd, text_content, strlen(text_content));
+		if (_write == -1)
+			return (-1);
+	}
 	close(fd);
 	return (1);
 
