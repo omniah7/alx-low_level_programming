c616ca3352c5ecbe4011c09b096b748efe22d275
omniah7 <om267165@gmail.com>
2022-07-29 14:45:18 +0200

    for task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,7 +4,7 @@
 * creat_file - creates a file
 * @filename: the name of the file to create
 * @text_content: a NULL terminated string to write to the file
-* Returns: 1 on success, -1 on failure
+* Return: 1 on success, -1 on failure
 * (file can not be created, file can not be written, write “fails”, etc…)
 */
 int create_file(const char *filename, char *text_content)
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 	if (text_content)
