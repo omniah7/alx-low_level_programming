9e93a7ad6c645ec814674b7fc70e07214580feab
omniah7 <om267165@gmail.com>
2022-12-08 19:30:52 +0200

    fix bug

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -9,6 +9,10 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	for (; index != 0; index--)
 	{
+		if (!head)
+		{
+			return NULL;
+		}
 		head = head->next;
 	}
 	return (head);
