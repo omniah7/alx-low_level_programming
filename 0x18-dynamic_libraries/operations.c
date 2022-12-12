df7d6b238b2dc3b7dae64217d093cc23b168d8f5
omniah7 <om267165@gmail.com>
2022-12-12 22:54:49 +0200

    create 100-operations.so

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,55 @@
+#include "operations.h"
+/**
+ * add - adds two integers
+ * @x: first number
+ * @y: second number
+ * Return: result
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
+
+/**
+ * sub - subtracts two integers
+ * @x: first number
+ * @y: second number
+ * Return: result
+ */
+int sub(int x, int y)
+{
+	return (x - y);
+}
+
+/**
+ * mul - multipies two integers
+ * @x: first number
+ * @y: second number
+ * Return: result
+ */
+int mul(int x, int y)
+{
+	return (x * y);
+}
+
+/**
+ * div - divides two integers
+ * @x: first number
+ * @y: second number
+ * Return: result
+ */
+int div(int x, int y)
+{
+	return (x / y);
+}
+
+/**
+ * mod - returns the remainder
+ * @x: first number
+ * @y: second number
+ * Return: result
+ */
+int mod(int x, int y)
+{
+	return (x % y);
+}
---
+++ 0x18-dynamic_libraries/operations.h
@@ -0,0 +1,8 @@
+#ifndef OPERATION_H
+#define OPERATIONS_H
+int add(int x, int y);
+int sub(int x, int y);
+int mul(int x, int y);
+int div(int x, int y);
+int mod(int x, int y);
+#endif
