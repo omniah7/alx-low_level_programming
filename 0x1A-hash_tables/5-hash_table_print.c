16229ccc0977fdf187f40257ae901bfd5bc8f10a
omniah7 <om267165@gmail.com>
2023-01-07 01:26:33 +0200

    tasks to 6

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,10 +1,35 @@
 #include "hash_tables.h"
 
-#include "hash_tables.h"
-/**\n* \n* \n*/
-/**\n* \n* \n*/
-/**\n* \n* \n*/
 /**
-* 
-* 
+* hash_table_print - prints a hash table.
+* @ht: the hash table
 */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned long int i;
+	char f;
+
+	f = 'f';
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		/*if empty list*/
+		if (!node)
+			continue;
+
+		/*print each element of the list*/
+		if (f != 'f')
+			printf(", ");
+		printf("'%s': '%s'", node->key, node->value);
+		f = 'n';
+		node = node->next;
+		while (node)
+		{
+			printf(", '%s': '%s'", node->key, node->value);
+			node = node->next;
+		}
+	}
+	printf("}\n");
+}
--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,9 +1,27 @@
 #include "hash_tables.h"
-#include "hash_tables.h"
-/**\n* \n* \n*/
-/**\n* \n* \n*/
-/**\n* \n* \n*/
 /**
-* 
-* 
+* hash_table_delete - deletes a hash table.
+* @ht: the hash table
 */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *node, *tmp;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		if (!node)
+			continue;
+		while (node)
+		{
+			tmp = node;
+			node = node->next;
+			free(tmp->key);
+			free(tmp->value);
+			free(tmp);
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
