f28b7dedd7866e99c311da17aaf3e432ba82b380
omniah7 <om267165@gmail.com>
2023-01-06 17:43:57 +0200

    task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,9 +1,31 @@
 #include "hash_tables.h"
-#include "hash_tables.h"
-/**\n* \n* \n*/
-/**\n* \n* \n*/
-/**\n* \n* \n*/
 /**
-* 
-* 
-*/
+ * hash_table_set - adds an element to the hash table.
+ *
+ * @ht: is the hash table you want to add or update the key/value to
+ * @key: the key
+ * @value: the value associated with the key. value must be duplicated.
+ * Return: 1 if it succeeded, 0 otherwise
+ * In case of collision, add the new node at the beginning of the list
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int idx;
+	hash_node_t *newnode;
+
+	newnode = malloc(sizeof(hash_node_t));
+	if (newnode == NULL)
+		return (0);
+	newnode->key = strdup(key);
+	newnode->value = strdup(value);
+	idx = key_index((unsigned char *)key, ht->size);
+	if (ht->array[idx] == NULL)
+	{
+		newnode->next = NULL;
+		ht->array[idx] = newnode;
+		return (1);
+	}
+	newnode->next = ht->array[idx];
+	ht->array[idx] = newnode;
+	return (1);
+}
--- 0x1A-hash_tables/3-main.c
+++ 0x1A-hash_tables/3-main.c
@@ -13,6 +13,7 @@ int main(void)
     hash_table_t *ht;
 
     ht = hash_table_create(1024);
-    hash_table_set(ht, "betty", "cool");
+    hash_table_set(ht, "hetairas collides with mentioner", "hi 1");
+    hash_table_set(ht, "depravement collides with serafins", "hi 2");
     return (EXIT_SUCCESS);
 }
