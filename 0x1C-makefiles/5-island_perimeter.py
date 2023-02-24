81311766d359e2dbebf867dc65a8a900a878480c
omniah7 <om267165@gmail.com>
2023-02-25 00:33:28 +0200

    makefiles

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+
+
+def island_perimeter(grid):
+    """returns the perimeter of the island described in grid"""
+    perimeter = 0
+    rowlength = len(grid)
+    colength = len(grid[0])
+    for row in range(rowlength):
+        for col in range(colength):
+            perimeter += vertical((row, col), rowlength, grid)
+            perimeter += horizontal((row, col), colength, grid)
+    return perimeter
+
+
+def vertical(pos, max, grid):
+    """return the perimeter of the vertical around a cell in grid
+    Attributes:
+        pos: a tuple of the posotion of the cell
+        max: the last indx of the rows in grid
+        grid: a list of list of integers
+    """
+    row, col = pos
+    # check the cell value
+    if grid[row][col] == 0:
+        return 0
+    p = 0
+
+    # check the cell above
+    if row != 0:
+        if grid[row - 1][col] == 0:
+            p += 1
+
+    # check the cell at bottom
+    if row != max:
+        if grid[row + 1][col] == 0:
+            p += 1
+
+    return p
+
+
+def horizontal(pos, max, grid):
+    """returns the perimeter of the horiozntal around a cell in grid
+    Attributes:
+        pos: a tuple of the posotion of the cell
+        max: the last indx of the columns in grid
+        grid: a list of list of integers
+    """
+    row, col = pos
+    # check the cell value
+    if grid[row][col] == 0:
+        return 0
+    p = 0
+
+    # check the left cell
+    if col != 0:
+        if grid[row][col - 1] == 0:
+            p += 1
+
+    # check the right cell
+    if col != max:
+        if grid[row][col + 1] == 0:
+            p += 1
+
+    return p
---
+++ 0x1C-makefiles/main.c
@@ -0,0 +1,14 @@
+#include "m.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 
+ */
+int main(void)
+{
+    // print_school();
+    printf("hello world");
+    return (EXIT_SUCCESS);
+}
+/* School */
